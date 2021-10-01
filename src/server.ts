ee8da151148b879f1b376699bfb09bbe7d74c389
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-01 06:07:08 -0300

    Iniciando a API

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 
 import { routes } from './routes';
+import swagerConfig from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swagerConfig));
 
 app.use(routes);
 
