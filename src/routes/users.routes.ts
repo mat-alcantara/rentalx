53534c2764174abd3e675c67b8709461aa919f31
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-05 07:48:15 -0300

    create upload config

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 export const usersRoutes = Router();
 
+const upload = multer({
+  dest: './tmp',
+});
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch('/avatar', updateUserAvatarController.handle);
