8db85661b3060abbd511126c5414520ff2af97fd
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-06 08:09:33 -0300

    Create usersRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,42 @@
+import { ICreateUsersDTO } from '@modules/accounts/dtos/ICreateUsersDTO';
+
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+    avatar,
+    id,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+      avatar,
+      id,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email);
+
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.users.find(user => user.id === id);
+
+    return user;
+  }
+}
