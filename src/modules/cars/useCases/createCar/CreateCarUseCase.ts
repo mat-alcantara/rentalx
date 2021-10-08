97bbdc7614500a5aaa89ec508f95f1ad70ddd73b
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-08 13:50:46 -0300

    add carAlreadyExist to createCar use case

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class CreateCarUseCase {
@@ -19,6 +20,22 @@ export class CreateCarUseCase {
     license_place,
     name,
   }: ICreateCarDTO): Promise<void> {
-    console.log('ok');
+    const carAlreadyExist = await this.carsRepository.findByLicensePlate(
+      license_place,
+    );
+
+    if (carAlreadyExist) {
+      throw new AppError('Car already exist');
+    }
+
+    await this.carsRepository.create({
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      license_place,
+      name,
+    });
   }
 }
