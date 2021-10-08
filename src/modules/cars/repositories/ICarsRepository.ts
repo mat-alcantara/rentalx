f0d97e2b92384094492615836469f5f7e9b40930
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-08 13:52:46 -0300

    add findCarByLicensePlate

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
+import { Car } from '../infra/typeorm/entities/Car';
 
 export interface ICarsRepository {
   create(carsData: ICreateCarDTO): Promise<void>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
 }
--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -6,6 +6,12 @@ import { ICarsRepository } from '../ICarsRepository';
 export class CarsRepositoryInMemory implements ICarsRepository {
   cars: Car[] = [];
 
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = this.cars.find(car => car.license_place === license_plate);
+
+    return car;
+  }
+
   async create({
     brand,
     category_id,
