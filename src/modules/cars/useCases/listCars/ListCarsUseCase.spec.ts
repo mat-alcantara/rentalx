c8f9e8a3e3d9c518736b4b2bd00043429a3a9dee
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-12 12:30:42 -0300

    finish ListCars tests

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -27,8 +27,46 @@ describe('List cars', () => {
     expect(carsAvailable).toEqual([car]);
   });
 
-  it('should be able to list all available cars by name', async () => {
-    await carsRepositoryInMemory.create({
+  it('should be able to list all available cars by name, brand and category_id', async () => {
+    const car1 = await carsRepositoryInMemory.create({
+      name: 'Car1',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand1',
+      category_id: 'category1',
+    });
+
+    const car2 = await carsRepositoryInMemory.create({
+      name: 'Car2',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand2',
+      category_id: 'category2',
+    });
+
+    const carsAvailableByName = await listCarsUseCase.execute({
+      name: car1.name,
+    });
+
+    const carsAvailableByBrand = await listCarsUseCase.execute({
+      brand: car2.brand,
+    });
+
+    const carsAvailableByCategoryId = await listCarsUseCase.execute({
+      category_id: car1.category_id,
+    });
+
+    expect(carsAvailableByName).toEqual([car1]);
+    expect(carsAvailableByBrand).toEqual([car2]);
+    expect(carsAvailableByCategoryId).toEqual([car1]);
+  });
+
+  it('should be able to list all available cars by name, brand and category_id', async () => {
+    const car = await carsRepositoryInMemory.create({
       name: 'Car1',
       description: 'Description Car',
       daily_rate: 100,
@@ -38,7 +76,7 @@ describe('List cars', () => {
       category_id: 'category',
     });
 
-    const car = await carsRepositoryInMemory.create({
+    await carsRepositoryInMemory.create({
       name: 'Car2',
       description: 'Description Car',
       daily_rate: 100,
