b971667bef1378edb5a82d32fd9859f27edb3992
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-12 12:43:59 -0300

    change request to use query in listAvailableCars

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsController.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsController.ts
@@ -5,16 +5,16 @@ import { ListAvailableCarsUseCase } from './ListAvailableCarsUseCase';
 
 export class ListAvailableCarsController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, brand, category_id } = request.body;
+    const { name, brand, category_id } = request.query;
 
     const listAvailableCarsUseCase = container.resolve(
       ListAvailableCarsUseCase,
     );
 
     const carsAvailable = await listAvailableCarsUseCase.execute({
-      name,
-      brand,
-      category_id,
+      name: name as string,
+      brand: brand as string,
+      category_id: category_id as string,
     });
 
     return response.json(carsAvailable);
