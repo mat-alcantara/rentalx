f9ce53919cddb5bed25720d58de0928f83333956
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-12 12:07:54 -0300

    make first test pass

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -2,16 +2,28 @@ import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/Car
 
 import { ListCarsUseCase } from './ListCarsUseCase';
 
-let carsRepository: CarsRepositoryInMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let listCarsUseCase: ListCarsUseCase;
 
 describe('List cars', () => {
   beforeEach(() => {
-    carsRepository = new CarsRepositoryInMemory();
-    listCarsUseCase = new ListCarsUseCase(carsRepository);
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
   });
 
   it('should be able to list all available cars', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car1',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+
     const carsAvailable = await listCarsUseCase.execute();
+
+    expect(carsAvailable).toEqual([car]);
   });
 });
